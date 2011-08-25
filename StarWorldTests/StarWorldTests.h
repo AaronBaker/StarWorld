535d26b36ca297860dbe64a05baf88290c3321e3
Aaron Baker <aaron.baker@imoutdoors.com>
2011-08-25 15:07:20 -0400

    Initial Commit

---
+++ StarWorldTests/StarWorldTests.h
@@ -0,0 +1,17 @@
+//
+//  StarWorldTests.h
+//  StarWorldTests
+//
+//  Created by Aaron Baker on 8/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface StarWorldTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ StarWorldTests/StarWorldTests.m
@@ -0,0 +1,33 @@
+//
+//  StarWorldTests.m
+//  StarWorldTests
+//
+//  Created by Aaron Baker on 8/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "StarWorldTests.h"
+
+
+@implementation StarWorldTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in StarWorldTests");
+}
+
+@end
